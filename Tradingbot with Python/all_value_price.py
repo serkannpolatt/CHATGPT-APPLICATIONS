5b1b5aeb0996dd74f7674146ce3bb0ec43c66f57
serkannpolatt <serkan.polat211@ogr.atauni.edu.tr>
2023-01-31 15:15:01 +0300

    .

--- Tradingbot with Python/all_value_price.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import json
-import websockets
-async def live_price():
-        async with websockets.connect( 'wss://stream.binance.com: 9443/ws/btcusdt@ticker') as websocket:
-            while True:
-                response = json. loads (await websocket.recv())
-                print (response)
-asyncio.get_event_loop().run_until_complete(live_price())
