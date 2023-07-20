8fd3000f2827543551c56223c035613fff3e0ed3
Serkan Polat <itsonlydatahustle@gmail.com>
2023-07-20 09:06:24 +0300

    main

---
+++ Automate Office Tasks using CHATGPT/06-Create Website/streamlit_app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+st.set_page_config(page_title="Automate Boring Office Tasks with ChatGPT and Python"
+                   
+                              "Sıkıcı Ofis Görevlerini ChatGPT ve Python ile Otomatikleştirin"  ,
+                  page_icon=":robot:",
+                  layout="centered")
+
+st.title("🤖 Automate Boring Office Tasks with ChatGPT and Python")
+st.title("🤖 Sıkıcı Ofis Görevlerini ChatGPT ve Python ile Otomatikleştirin")
+
+
+st.markdown("Learn how to use ChatGPT and Python to automate tasks such as distributing Excel files with Outlook, creating interactive charts from Excel, merging PDFs, and more.")
+st.markdown("Excel dosyalarını Outlook ile dağıtma, Excel'den etkileşimli grafikler oluşturma, PDF'leri birleştirme ve daha fazlası gibi görevleri otomatikleştirmek için ChatGPT ve Python'u nasıl kullanacağınızı öğrenin.")
+
+st.markdown("""
+<a href="https://chatgpt-static.s3.amazonaws.com/chats/tx61280.html" target="_blank" style="font-size:20px; font-weight:bold;">👉 Distribute Excel Files With Outlook / Outlook ile Excel Dosyalarını Dağıtın</a>
+<br>
+<a href="https://chatgpt-static.s3.amazonaws.com/chats/wj70814.html" target="_blank" style="font-size:20px; font-weight:bold;">👉 Automate PowerPoint / PowerPoint'i Otomatikleştirin</a>
+<br>
+<a href="https://chatgpt-static.s3.amazonaws.com/chats/ln81825.html" target="_blank" style="font-size:20px; font-weight:bold;">👉 Create Interactive Charts From Excel / Excel'den Etkileşimli Grafikler Oluşturun </a>
+<br>
+<a href="https://chatgpt-static.s3.amazonaws.com/chats/uf98915.html" target="_blank" style="font-size:20px; font-weight:bold;">👉 Merge PDFs / PDF'leri birleştir</a>
+<br>
+<a href="https://chatgpt-static.s3.amazonaws.com/chats/qq32478.html" target="_blank" style="font-size:20px; font-weight:bold;">👉 Write E-Mails / Email Yaz</a>
+<br>
+<a href="https://chatgpt-static.s3.amazonaws.com/chats/aa18460.html" target="_blank" style="font-size:20px; font-weight:bold;">👉 How I created this website / Bu web sitesini nasıl oluşturdum</a>
+""", unsafe_allow_html=True)
+
